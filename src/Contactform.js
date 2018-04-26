9da37400bf7180390a19886e3ed4d2e3344db78c
Dominika Krawczyk <krawczyk_d@wp.pl>
2018-04-26 16:52:09 +0200

    ContactForm component

---
+++ src/Contactform.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class ContactForm extends Component {
+  render() {
+    return (
+      <div>
+        <input/>
+        <button>Send</button>
+      </div>
+    )
+  }
+}
+
+export default ContactForm
