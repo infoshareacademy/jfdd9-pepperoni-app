1c1628d7b24154283bcf86c4eca50ec265cb51f0
Nina Gostanska <your.name@domainninagorska@gmail.com>
2018-04-26 15:54:16 +0200

    Main Search Engine router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import MainSearchEngine from "./MainSearchEngine";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router>
+        <div>
 
-      </div>
+
+          <Route exact path="/" component={MainSearchEngine}/>
+          {/*<Route path="/about" component={TasksView}/>*/}
+          {/*<Route path="/tasks" component={TasksView}/>*/}
+          {/*<Route path="/magic-number/:number" component={MagicNumber}/>*/}
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/MainSearchEngine.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class MainSearchEngine extends React.Component {
+  render() {
+    return (
+      <h2 >Main Search Engine
+      </h2>
+    )
+  }
+}
+
+export default MainSearchEngine
