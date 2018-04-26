cdd100d479300fffe916e477300fff1f326d9fdf
Nina Gostanska <your.name@domainninagorska@gmail.com>
2018-04-26 16:51:14 +0200

    Fetching gangsters

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,11 +1,36 @@
 import React from 'react'
 
 class SearchResults extends React.Component {
+
+  state = {
+    gangsters: null
+  }
+
+  static defaultProps = {
+    hometown: 'Gdańsk'
+  }
+
+  componentDidMount() {
+    fetch(process.env.PUBLIC_URL + '/gangsterDatabase.json').then(
+      response => response.json()
+    ).then(
+      gangsters => this.setState({ gangsters: gangsters })
+    )
+  }
   render() {
+    debugger
     return (
         <div >
-          <img src={process.env.PUBLIC_URL + '/gangster.jpg'}
           <h2>Client 1</h2>
+          {
+            this.state.gangsters === null
+              ? 'Ładuję gangusów'
+              : this.state.gangsters.filter(
+                gangster => gangster.hometown === this.props.hometown
+              ).map(
+                gangster => <p>{gangster.first_name}</p>
+              )
+          }
         </div>
     )
   }
