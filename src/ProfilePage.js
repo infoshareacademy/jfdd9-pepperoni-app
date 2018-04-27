d717a73ab90919a5c7926a5926aa0863e5fe53fd
Patryk Czupryn <your.paczu1991@gmail.com>
2018-04-27 15:42:15 +0200

    state done fetch

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
 class ProfilePage extends React.Component {
-  render() {
-    return (
-      <h2>Profile
-      </h2>
-  )
-  }
+
+
   state = {
+    gangsters: null
+  }
+
+  static defaultProps = {
+    hometown: 'Gdańsk'
+  }
+
+  componentDidMount() {
+    fetch(process.env.PUBLIC_URL + '/gangsterDatabase.json').then(
+      response => response.json()
+    ).then(
+      gangsters => this.setState({gangsters: gangsters})
+    )
+  }
+
+ /* state = {
     id: '',
     first_name: '',
     rating: '',
@@ -20,8 +32,26 @@ class ProfilePage extends React.Component {
     description: '',
     experience: '',
     opinions: '',
-  }
+  } */
 
+  render() {
+    return (
+      <div>
+        <h2>Profile
+        </h2>
+        {
+          this.state.gangsters === null
+            ? 'Ładuję gangusów'
+            : this.state.gangsters.filter(
+            gangster => gangster.hometown === this.props.hometown
+            ).map(
+            gangster => <p>{gangster.first_name}</p>
+            )
+        }
+      </div>
+
+    )
+  }
 }
 
 export default ProfilePage
