f16abba569d0e4baecac1109d7be96a40177a91f
Patryk Czupryn <your.paczu1991@gmail.com>
2018-04-27 16:04:42 +0200

    state done fetch single profile

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -4,7 +4,7 @@ class ProfilePage extends React.Component {
 
 
   state = {
-    gangsters: null
+    gangster: null
   }
 
   static defaultProps = {
@@ -15,37 +15,47 @@ class ProfilePage extends React.Component {
     fetch(process.env.PUBLIC_URL + '/gangsterDatabase.json').then(
       response => response.json()
     ).then(
-      gangsters => this.setState({gangsters: gangsters})
+      gangsters => this.setState({gangster: gangsters.find(gangster => gangster.id.toString() === this.props.match.params.gangsterId)})
     )
   }
 
- /* state = {
-    id: '',
-    first_name: '',
-    rating: '',
-    gender: '',
-    email: '',
-    image: '',
-    hometown: '',
-    availability: '',
-    tags: '',
-    description: '',
-    experience: '',
-    opinions: '',
-  } */
+  /* state = {
+     id: '',
+     first_name: '',
+     rating: '',
+     gender: '',
+     email: '',
+     image: '',
+     hometown: '',
+     availability: '',
+     tags: '',
+     description: '',
+     experience: '',
+     opinions: '',
+   } */
 
   render() {
+    const gangster = this.state.gangster
     return (
       <div>
         <h2>Profile
         </h2>
         {
-          this.state.gangsters === null
-            ? 'Ładuję gangusów'
-            : this.state.gangsters.filter(
-            gangster => gangster.hometown === this.props.hometown
-            ).map(
-            gangster => <p>{gangster.first_name}</p>
+          this.state.gangster === null
+            ? 'Ładuję gangusa'
+            : (
+              <div>
+                <p>{gangster.first_name}</p>
+                <p>{gangster.ratinge}</p>
+                <p>{gangster.gender}</p>
+                <p>{gangster.email}</p>
+                <p>{gangster.hometown}</p>
+                <p>{gangster.availbility}</p>
+                <p>{gangster.tags}</p>
+                <p>{gangster.description}</p>
+                <p>{gangster.experiance}</p>
+                <p>{gangster.opinions}</p>
+              </div>
             )
         }
       </div>
