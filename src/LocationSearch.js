adc904e45ea2f0fb7955f99a08d5ff9deac1a1e3
Paulina Chojnowska <antaria@vp.pl>
2018-04-27 16:36:01 +0200

    Search form by location

--- src/LocationSearch.js
+++ src/LocationSearch.js
@@ -1,83 +1,34 @@
 import React from 'react'
-import gangsterDatabase from '../public/gangsterDatabase.json'
+
 
 class LocationSearch extends React.Component {
   state ={
-    filteredList: []
+    hometown: ''
   }
 
-  filterDatabase =(event) => {
-    const updatedList = gangsterDatabase.filter(city => city === event.target.value)
-    console.log(updatedList)
+  handleSubmit = event => {
+    event.preventDefault()
+
+    this.props.addCity(this.state.hometown)
   }
+
+  handleChange = event => {
+    this.setState({
+      hometown: event.target.value
+    })
+  }
+
   render() {
     return (
-      <div>
-        <form>
-            <fieldset className="form-group">
-              <input type="text" placeholder="Enter city" onChange={this.filterDatabase}/>
-            </fieldset>
-        </form>
+      <React.Fragment>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Enter city" name="hometown" value={this.state.hometown} onChange={this.handleChange}/>
+          </form>
         </div>
+      </React.Fragment>
     )
   }
 }
 
 export default LocationSearch
-
-// var FilteredList = React.createClass({
-//   filterList: function(event){
-//     var updatedList = this.state.initialItems;
-//     updatedList = updatedList.filter(function(item){
-//       return item.toLowerCase().search(
-//         event.target.value.toLowerCase()) !== -1;
-//     });
-//     this.setState({items: updatedList});
-//   },
-//   getInitialState: function(){
-//     return {
-//       initialItems: [
-//         "Apples",
-//         "Broccoli",
-//         "Chicken",
-//         "Duck",
-//         "Eggs",
-//         "Fish",
-//         "Granola",
-//         "Hash Browns"
-//       ],
-//       items: []
-//     }
-//   },
-//   componentWillMount: function(){
-//     this.setState({items: this.state.initialItems})
-//   },
-//   render: function(){
-//     return (
-//       <div className="filter-list">
-//         <form>
-//           <fieldset className="form-group">
-//             <input type="text" className="form-control form-control-lg" placeholder="Search" onChange={this.filterList}/>
-//           </fieldset>
-//         </form>
-//         <List items={this.state.items}/>
-//       </div>
-//     );
-//   }
-// });
-//
-// var List = React.createClass({
-//   render: function(){
-//     return (
-//       <ul className="list-group">
-//         {
-//           this.props.items.map(function(item) {
-//             return <li className="list-group-item" data-category={item} key={item}>{item}</li>
-//           })
-//         }
-//       </ul>
-//     )
-//   }
-// });
-//
-// React.render(<FilteredList/>, document.getElementById('app'));
--- src/MainSearchEngine.js
+++ src/MainSearchEngine.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import LocationSearch from "./LocationSearch";
+import SearchResults from "./SearchResults";
 
 class MainSearchEngine extends React.Component {
+
+  state = {
+    hometown: ''
+  }
+
+  addCity = (cityName) => {
+    this.setState({
+      hometown: cityName
+      }
+    )
+  }
+
   render() {
     return (
       <React.Fragment>
-      <h2 >Main Search Engine
+      <h2>Search for gangsters...
       </h2>
-        <LocationSearch/>
+        <LocationSearch addCity={this.addCity}/>
+        <SearchResults hometown={this.state.hometown}/>
       </React.Fragment>
     )
   }
