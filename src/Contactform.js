ecdf173464f936b6f427b0977f171c90e63f4118
Dominika Krawczyk <krawczyk_d@wp.pl>
2018-05-08 15:42:38 +0200

    email field -update

--- src/Contactform.js
+++ src/Contactform.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 
 class ContactForm extends Component {
+ handleSubmit = event =>{
+   event.preventDefault();
+   console.log('submit');
+ };
+
   render() {
     return (
       <div>
-        <input name="email"/>
+        <form onSubmit={this.handleSubmit}>
+        <input name="email" value="email address"/>
         <button>Send</button>
+        <div>
+          <textarea></textarea>
+        </div>
+        </form>
       </div>
     )
   }
 }
 
-export default ContactForm
+export default ContactForm;
