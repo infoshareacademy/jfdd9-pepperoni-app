94fd90d16b8514bac5d0b68ea5ff840dedf37c6b
Paulina Chojnowska <antaria@vp.pl>
2018-05-12 19:26:58 +0200

    Added GTA-like talking police heads in upper right corner of Thank You Page

--- src/ThankYouPage.js
+++ src/ThankYouPage.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import logo from "./police.jpg"
+import logoHead from "./police-head.gif"
 
 const Style = {
+  width: '80%',
   fontSize: '1.2rem',
   marginLeft: '100px',
   // textAlign: 'center',
 }
 const policeStyle = {
   height: '60px',
-  margin: '0 auto'
+  margin: '0 auto',
+}
+
+const policeHeadStyle = {
+  float: 'right',
+  margin: '20px 0'
 }
 
 class ThankYouPage extends React.Component {
@@ -28,8 +35,15 @@ class ThankYouPage extends React.Component {
   render() {
     return (
       <div style={Style}>
-      <h1 >Thank You!
-      </h1>
+        {this.state.surprise &&
+        <div>
+          <img style={policeHeadStyle} src={logoHead} alt="Police head"/>
+          <img style={policeHeadStyle} src={logoHead} alt="Police head"/>
+          <img style={policeHeadStyle} src={logoHead} alt="Police head"/>
+          <img style={policeHeadStyle} src={logoHead} alt="Police head"/>
+          <img style={policeHeadStyle} src={logoHead} alt="Police head"/>
+        </div>}
+      <h1>Thank You!</h1>
       <h2>You have successfully submitted your order...</h2>
         {this.state.surprise &&
         <div>
