8adc8f103c93369b37d517dcf62dee6c9c426217
Paulina Chojnowska <antaria@vp.pl>
2018-05-16 15:50:13 +0200

    Added firebase configs & new Firebase-like file with gangster database

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDL_-ceGidXs1vTjycuur5jQidgPHUqHic",
+  authDomain: "pepperoni-app.firebaseapp.com",
+  databaseURL: "https://pepperoni-app.firebaseio.com",
+  projectId: "pepperoni-app",
+  storageBucket: "pepperoni-app.appspot.com",
+  messagingSenderId: "450500818623"
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import setupFirebase from './setupFirebase'
+
+setupFirebase()
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
