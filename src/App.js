035b7accecf2ea0f59cfcd36cd14c6f95217bccc
Nina Gostanska <your.name@domainninagorska@gmail.com>
2018-05-16 16:30:49 +0200

    LogIn - WIP

--- src/App.js
+++ src/App.js
@@ -7,24 +7,39 @@ import OrderPage from "./OrderPage";
 import ProfilePage from "./ProfilePage";
 import ThankYouPage from "./ThankYouPage";
 import NavBar from "./NavBar";
-import GangstersForTag from "./GangstersForTag"
+import GangstersForTag from "./GangstersForTag";
+import SignInForm from "./SignInForm";
+import SignUpForm from "./SignUpForm";
 
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <NavBar/>
-          <Route exact path="/" component={MainSearchEngine}/>
-          <Route path="/search-results" component={SearchResults}/>
-          <Route path="/profile/:gangsterId" component={ProfilePage}/>
-          <Route path="/order/:gangsterId/:selectedDate" component={OrderPage}/>
-          <Route path="/thank-you" component={ThankYouPage}/>
-          <Route path="/gangsters-for-tag/:tagName" component={GangstersForTag}/>
-        </div>
-      </Router>
-    );
+      <div>
+        {
+          this.props.user === null ? (
+            <div>
+              <SignInForm/>
+              <SignUpForm/>
+            </div>
+          ) : (
+            <Router>
+              <div className="App">
+                <NavBar/>
+                <p>
+                  logged in as: {this.props.user.email}) <button onClick={this.props.signOut}>Sign out</button>
+                </p>
+                <Route exact path="/" component={MainSearchEngine}/>
+                <Route path="/search-results" component={SearchResults}/>
+                <Route path="/profile/:gangsterId" component={ProfilePage}/>
+                <Route path="/order/:gangsterId/:selectedDate" component={OrderPage}/>
+                <Route path="/thank-you" component={ThankYouPage}/>
+                <Route path="/gangsters-for-tag/:tagName" component={GangstersForTag}/>
+              </div>
+            </Router>)
+        }
+      </div>
+    )
   }
 }
 
