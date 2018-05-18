366fca8c31989cdc115627860ffc6835ccc9f700
Paulina Chojnowska <antaria@vp.pl>
2018-05-18 16:53:42 +0200

    Beginnings of My Profile Page

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import moment from 'moment'
 import Calendar from "./Calendar";
 import StarsRating from "./StarsRating";
 import {withGangsters} from "./contexts/Gangsters";
