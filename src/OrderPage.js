1d64392608217d63ce934b022ffe3120d8f9cd52
Patryk Czupryn <your.paczu1991@gmail.com>
2018-05-21 14:58:37 +0200

    order page fix

--- src/OrderPage.js
+++ src/OrderPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ContactForm from "./Contactform";
 import moment from 'moment';
 import firebase from 'firebase'
+import {withGangsters} from "./contexts/Gangsters";
 
 const orderPageStyle = {
   width: '80%',
@@ -14,43 +15,27 @@ class OrderPage extends React.Component {
     selectedTag: ''
   }
 
-  componentDidMount() {
-    firebase.database().ref('/gangsters').once('value').then(
-      snapshot => this.setState({
-        gangster: Object.entries(snapshot.val() || {}).map(([id, rest]) => ({id, ...rest})).find(gangster => gangster.id.toString() === this.props.match.params.gangsterId)
-      })
-    )
-
-
-    // fetch(process.env.PUBLIC_URL + '/gangsterDatabase.json').then(
-    //   response => response.json()
-    // ).then(
-    //   gangsters => this.setState({gangster: gangsters.find(gangster => gangster.id.toString() === this.props.match.params.gangsterId)})
-    // )
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return {
+      gangster: nextProps.gangsters.data && nextProps.gangsters.data.find(
+        gangster => gangster.id.toString() === nextProps.match.params.gangsterId
+      )
+    }
   }
 
-  handleTagChange = event => {
-    this.setState({
-      selectedTag: event.target.value
-    })
-  }
+
 
 
   render() {
-    var date = moment.unix((this.props.match.params.selectedDate/1000)).format("DD/MM/YYYY");
     return (
       <div style={orderPageStyle}>
         {
           this.state.gangster === null
           ? 'Loading order'
           : (
-            <div>
-              <h1>Your order</h1>
-              <h2>Gangster: {this.state.gangster.first_name}</h2>
-              <h2>Date: {date}</h2>
-              <h2>Job: {this.state.selectedTag}</h2>
-              <ContactForm tags={this.state.gangster.tags} handleTagChange={this.handleTagChange}/>
-            </div>
+
+              <ContactForm gangster={this.state.gangster}/>
+
             )
         }
       </div>
@@ -58,4 +43,4 @@ class OrderPage extends React.Component {
   }
 }
 
-export default OrderPage
+export default withGangsters(OrderPage)
