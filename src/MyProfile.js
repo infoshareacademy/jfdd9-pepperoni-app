29371261592b702cf8c888e37a9c57ed5b71ae90
Paulina Chojnowska <antaria@vp.pl>
2018-05-21 14:52:07 +0200

    My Profile Page - work in progress

--- src/MyProfile.js
+++ src/MyProfile.js
@@ -2,17 +2,32 @@ import React from 'react'
 import StarsRating from "./StarsRating";
 import { withGangsters } from "./contexts/Gangsters";
 import { withUser} from "./User";
+import './profile.css'
+import firebase from 'firebase'
+import MyProfileForm from "./MyProfileForm";
 
-class MyProfilePage extends React.Component {
+class MyProfile extends React.Component {
+  state = {
+    gangster: null,
+  }
 
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const gangsters = nextProps.gangsters.data
+    return {
+      gangster: gangsters.find(gangster => gangster.id.toString() === firebase.auth().currentUser.user.uid)
+    }
+  }
 
+render() {
 
+  <MyProfileForm/>
 
-
+    return
+}
 
 
 
 }
 
 
-export default withUser(withGangsters(ProfilePage))
+export default withUser(withGangsters(MyProfile))
---
+++ src/MyProfileForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class MyProfileForm extends React.Component {
+
+
+  render() {
+    return (
+      <React.Fragment>
+      <h1>Complete your profile to become a gangster</h1>
+
+      </React.Fragment>
+    )
+  }
+
+
+}
+
+export default MyProfileForm
