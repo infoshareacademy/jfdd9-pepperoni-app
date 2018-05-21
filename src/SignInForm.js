ae3ae6b59e2e776d492fa0e7146ee5d1f117533d
Nina Gostanska <your.name@domainninagorska@gmail.com>
2018-05-21 15:26:20 +0200

    style/CSS wip

--- src/SignInForm.js
+++ src/SignInForm.js
@@ -1,6 +1,32 @@
 import React, { Component } from 'react'
 import { withUser } from './User';
 
+// const listStyle = {
+//   fontSize: '25px',
+//   width: '80%',
+//   display: 'inline-block',
+//   textDecoration: 'none',
+//   marginTop: '26px',
+//   marginBottom: '0px',
+//   verticalAlign: 'top',
+//   color: '#ececec',
+// }
+
+const formSectionLayer = {
+  maxWidth: '40rem',
+  margin: '0 auto',
+  padding: '1rem',
+  textAlign: 'center',
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  lineHeight: '2rem',
+  border: 'solid 1px rgba(31, 31, 31, 0.83)',
+  borderRadius: '10px',
+  backgroundColor: 'rgba(15, 15, 15, 0.83)',
+}
+
 class SignInForm extends Component {
 
   state = {
@@ -21,22 +47,24 @@ class SignInForm extends Component {
 
   render() {
     return (
-      <div>
-        <h2>Sign in</h2>
+      <div style={formSectionLayer}>
+        <h2>Welcome text</h2>
         {this.props.signInError && <p>{this.props.signInError.message}</p>}
         <form onSubmit={this.handleSubmit}>
           <input
             value={this.state.username}
             name="username"
             type="text"
+            placeholder="Username"
             onChange={this.handleChange}
-          />
+          /><br/>
           <input
             value={this.state.password}
             name="password"
             type="password"
+            placeholder="Your Email"
             onChange={this.handleChange}
-          />
+          /><br/>
           <button>sign in</button>
         </form>
       </div>
