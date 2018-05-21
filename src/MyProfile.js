2e6ff1b3f81132c2709fff12f0ff2739ec6547b6
Dominika Krawczyk <krawczyk_d@wp.pl>
2018-05-21 14:51:40 +0200

    gangster automatically added to database - SignInForm

--- src/MyProfile.js
+++ src/MyProfile.js
@@ -2,17 +2,20 @@ import React from 'react'
 import StarsRating from "./StarsRating";
 import { withGangsters } from "./contexts/Gangsters";
 import { withUser} from "./User";
+import firebase from 'firebase'
 
 class MyProfilePage extends React.Component {
 
-
-
-
-
-
+  render() {
+    return (
+      <div>
+        <h1>{firebase.auth().currentUser.email}</h1>
+      </div>
+    )
+  }
 
 
 }
 
 
-export default withUser(withGangsters(ProfilePage))
+export default withUser(withGangsters(MyProfilePage))
